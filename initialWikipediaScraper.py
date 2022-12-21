9924fffdcb1d3edbd3f1348e75a503680b586659
Exceed IO Inc <arthurlee087@gmail.com>
2022-12-20 18:59:12 -0600

    Update initialWikipediaScraper.py

--- initialWikipediaScraper.py
+++ initialWikipediaScraper.py
@@ -1,38 +1,62 @@
-def buildStringCombos(string: str):
-    stringSplices = list()
-    for index in range(len(string)):
-        for index2 in range(len(string)):
-            if string[index:index2]:
-                if string[index:index2+1] not in stringSplices:
-                    stringSplices.append(string[index:index2+1])
-    strings = []
-    for i in range(0, len(stringSplices)-1, 2):
-        strings.append(stringSplices[i+1])
-        strings.append(stringSplices[i])
-    return strings
+import models
+from selenium import webdriver
+import chromedriver_autoinstaller
+chromedriver_autoinstaller.install()
 
 
-def removeDuplicateString(string: str):
-    print(f"FUNCTION removeDuplicateString ENTER__________________{string}")
-    stringCombos = buildStringCombos(string)[0]
-    proposals = list()
-    for target in stringCombos:
-        scanned = string
-        reversed_scan = list(scanned)
-        reversed_scan.reverse()
-        reversed_scan = "".join(reversed_scan)
-        reversed_target = list(target)
-        reversed_target.reverse()
-        reversed_target = "".join(reversed_target)
-        print(f"\nTarget: {target}\nScanned: {scanned}\nReversed: {reversed_scan}\nReversed Target: {reversed_target}")
-        proposal = list(reversed_scan.replace(reversed_target, "", 1))
-        proposal.reverse()
-        proposal = "".join(proposal)
-        proposals.append(proposal)
-        return proposal
-    proposals = list(proposals)
-    proposals.sort(key=lambda x: len(x))
-    return proposals
+def normalizeStringChars(string: str):
+    string = list(string)
+    stringer = []
+    for char in string:
+        if char.isalpha() or char.isdigit() or char in [",", "(", ")", " ", "!"]:
+            stringer.append(char)
+    print("".join(stringer))
+    return "".join(stringer)
 
 
-print(removeDuplicateString("9questMappin our 9questMan"))
+def getAllOriginalWorkTitles():
+    driver = webdriver.Chrome()
+    for year in range(2022, 1945, -1):
+        urlForData = ""
+        if year > 1957:
+            urlForData = f"https://en.wikipedia.org/wiki/Billboard_Year-End_Hot_100_singles_of_{year}"
+        elif year > 1955:
+            urlForData = f"https://en.wikipedia.org/wiki/Billboard_year-end_top_50_singles_of_{year}"
+        elif year > 1948:
+            urlForData = f"https://en.wikipedia.org/wiki/Billboard_year-end_top_30_singles_of_{year}"
+        elif year > 1939:
+            urlForData = f"https://en.wikipedia.org/wiki/Billboard_year-end_top_singles_of_{year}"
+        driver.get(urlForData)
+        source = driver.page_source.split("title=")
+        for source_ in source:
+            source__ = source_.split("\n")
+            for source___ in source__:
+                if "</a>" in source___ and (len(source___) < 50):
+                    print(normalizeStringChars(source___), len(source___), source___)
+
+
+    driver.quit()
+
+
+def countCharacter(char: str, string: str):
+    count = 0
+    for letter in string:
+        if char[0].lower() == letter.lower():
+            count += 1
+    return count
+
+
+def removeDuplicates(string: str):
+    if len(string) > 1:
+        for outerCharIndex in range(len(string)-1):
+            for innerCharIndex in range(1, len(string)):
+                stringSmaller = string[outerCharIndex:innerCharIndex]
+                if stringSmaller in string[innerCharIndex:]:
+                    string = string.replace(stringSmaller, ""
+                                                           "")
+    return string
+
+
+#print(removeDuplicates('"Lauren Alaina">Lauren Alaina</a>'))
+getAllOriginalWorkTitles()
+
